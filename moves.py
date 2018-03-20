b2d4cdd0e98257cf676773608b4a44ae851982c7
Kenny Lee <kennyl2@student.unimelb.edu.au>
2018-03-21 00:22:25 +1100

    Commented

--- moves.py
+++ moves.py
@@ -2,29 +2,50 @@
 # Created by JiaWei and Kenny (20/3/18)
 
 
-
 def count_legal_move(new_board, color):
     count = 0
+
+    # For every Piece in the Board's Piece List
     for piece in new_board.pieces:
+
+        # Check if the Piece has the desired Color
         if piece.color is color:
+
+            # Check the Piece's Direction (TOP, BOTTOM, LEFT, RIGHT)
+            # has an Object
+            # Since there are CORNERS and Pieces on the edge do not necessarily
+            # have neighbours
+
+            # If the Piece's LEFT has an Object
             if piece.left:
+
+                # Check if the Object is a Square
                 if isinstance(piece.left, Square):
                     count += 1
+
                 else:
+                    # If not, check if the Piece's Left Left is a Square
+                    # Since they can jump
                     if piece.left.left and isinstance(piece.left.left, Square):
                         count += 1
+
+
             if piece.right:
                 if isinstance(piece.right, Square):
                     count += 1
                 else:
                     if piece.right.right and isinstance(piece.right.right, Square):
                         count += 1
+
+
             if piece.top:
                 if isinstance(piece.top, Square):
                     count += 1
                 else:
                     if piece.top.top and isinstance(piece.top.top, Square):
                         count += 1
+
+
             if piece.bottom:
                 if isinstance(piece.bottom, Square):
                     count += 1

959cc333dd617d6b72a693a059bff10a086d237f
Kenny Lee <kennyl2@student.unimelb.edu.au>
2018-03-24 17:31:47 +1100

    Added new functions in moves.py

--- moves.py
+++ moves.py
@@ -61,57 +61,61 @@ def check_move(piece, dir, target_color):
     # First check if the move is possible
     if check_valid_move(piece, dir):
         # Check if move could be eliminated
-        if check_elimination_move(piece, dir, target_color):
+        if not move_is_eliminated(piece, dir, target_color):
             return YES
 
     else:
         # Check if a jump move is possible
-        if check_valid_jump_move(piece, dir):
+        if check_valid_jump_move(piece, dir, target_color):
             return JUMP
 
     return NO
 
 
 # This function checks if the a move results in the elimination of the Piece
-def check_elimination_move(piece, dir, target_color):
+def move_is_eliminated(piece, dir, target_color):
 
     if dir == RIGHT:
         if isinstance(piece.top_right, Piece) and isinstance(piece.bottom_right, Piece):
             if piece.top_right.color == target_color and piece.bottom_right == target_color:
-                return False
+                return True
 
     if dir == LEFT:
         if isinstance(piece.top_left, Piece) and isinstance(piece.bottom_left, Piece):
             if piece.top_left.color == target_color and piece.bottom_left.color == target_color:
-                return False
+                return True
 
     if dir == TOP:
         if isinstance(piece.top_left, Piece) and isinstance(piece.top_right, Piece):
             if piece.top_left.color == target_color and piece.top_right.color == target_color:
-                return False
+                return True
 
     if dir == BOTTOM:
         if isinstance(piece.bottom_left, Piece) and isinstance(piece.bottom_right, Piece):
             if piece.bottom_left.color == target_color and piece.bottom_right.color == target_color:
-                return False
+                return True
 
-    return True
+    return False
 
-# This function returns if a specific direction could jumped over
-def check_valid_jump_move(piece, dir):
+# This function returns if a specific direction could be jumped over
+# and not get eliminated
+def check_valid_jump_move(piece, dir, target_color):
 
     if dir == LEFT and isinstance(piece.left.left, Square):
-        return True
+        if not move_is_eliminated(piece.left, LEFT, target_color):
+            return True
 
     if dir == RIGHT and isinstance(piece.right.right, Square):
-        return True
+        if not move_is_eliminated(piece.left, RIGHT, target_color):
+            return True
 
     if dir == TOP and isinstance(piece.top.top, Square):
-        return True
+        if not move_is_eliminated(piece.top, TOP, target_color):
+            return True
 
     if dir == BOTTOM and isinstance(piece.bottom.bottom, Square):
-        return True
-
+        if not move_is_eliminated(piece.bottom, BOTTOM, target_color):
+            return True
 
     return False
 

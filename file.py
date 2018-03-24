8a89bb7b2c0cbbe23e16646670dab018466960ce
Kenny <kennylee@Kennys-iMac.local>
2018-03-25 02:20:40 +1100

    Added Moves and Massacre functionality

--- file.py
+++ file.py
@@ -3,16 +3,12 @@
 
 from board import *
 
-
-# Still have to implement a function that takes in either Moves or Massacre
-
-
 # Reads the board configuration from a file and recreates the actual Board
 def read_file(file_name, new_board):
 
     # Opens up the file and read the lines of the data (Board Config)
     # Fitting each row of data to an Array (content[i])
-    with open(file_name) as f:
+    with open(file_name, 'r') as f:
         content = f.readlines()
 
     # Gets rid of all the '\n' char from the data
@@ -23,6 +19,11 @@ def read_file(file_name, new_board):
 
         # Split up the individual characters e.g. '-, X, O, @' of each row
         content[x] = ''.join(content[x].split())
+
+        # check if the line contains Massacre or Moves
+        if content[x] == MASSACRE or content[x] == MOVES:
+            OUTPUT_STATE = content[x]
+            break
 
         # For every character within a row
         for y in range(len(content[x])):
@@ -38,4 +39,4 @@ def read_file(file_name, new_board):
             elif content[x][y] == '@':
                 new_board.add_to_pieces(Piece(x, y, BLACK))
 
-        print(content[x])
+    return OUTPUT_STATE
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from board import *
 from file import *
 from moves import *
 from goal import *
+import sys
 
 # This is the main file
 # Created by JiaWei and Kenny (20/3/18)
@@ -12,17 +13,21 @@ def main():
     # Create a new Board
     board = Board()
 
+    file = sys.argv[1]
+
     # Read the file and set up the Board with Squares and Pieces
-    read_file('sample3.txt', board)
+    OUTPUT_STATE = read_file(file, board)
 
     # Find the neighbours of every Pieces
     find_neighbour(board)
 
-    print(count_legal_move(board, WHITE))
-    print(count_legal_move(board, BLACK))
+    # Output depending on the Messsage
+    if OUTPUT_STATE == MASSACRE:
+        determinate_goals(board, BLACK)
 
-    goal_squares = []
-    determinate_goals(board, BLACK)
+    elif OUTPUT_STATE == MOVES:
+        print(count_legal_move(board, WHITE))
+        print(count_legal_move(board, BLACK))
 
 
 

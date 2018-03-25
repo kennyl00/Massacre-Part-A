1e6d0e81d7035553f20efb4e5170f095d499b6e4
Jiawei Xu <jiaweix2@student.unimelb.edu.au>
2018-03-25 12:58:49 +1100

    massacre

---
+++ Massacre.py
@@ -0,0 +1,25 @@
+from board import *
+
+def Massacre(goal_square, new_board):
+    # create a suqare list according to goal_square
+
+    while i < len(
+        if either is occupied by black:
+            goal_square.append(goals.pop())
+            continue
+
+            else:
+                if check_move(goals.piece1):
+                    if check_move(goals.piece2):
+                        astar
+
+                    else:
+                        goal_square.append(goals.pop())
+
+
+                elif check_move(goals.piece2):
+                    if check_move(goals.piece1):
+                        astar
+
+                    else:
+                    goal_square.append(goals.pop())
